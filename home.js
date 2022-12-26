5db41f7a6ab2281154a1b69ed339f47df658bf5f
ErmiraMaliqi <ermiramaliqi00@gmail.com>
2022-12-26 20:40:53 +0100

    add about-us, our-blog and contact-us

--- home.js
+++ home.js
@@ -19,7 +19,21 @@ function showSlides() {
 }
 
 
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("read-more");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
 
 
 
